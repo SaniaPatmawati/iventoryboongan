2ef050e6116dd2041b93e033dce3e7eadd538fa0
Your Name <you@example.com>
2019-05-23 16:34:35 +0700

    add file master

---
+++ application/views/user/index.php
@@ -0,0 +1,51 @@
+<style>
+	 body {
+      font-family: "Open Sans", sans-serif;
+      height: 100vh;
+      background: url("assets/image/kpu.jpg") 50% fixed;
+      background-size: cover;
+    }
+</style>
+
+
+<br><br><br>
+<body>
+	 <div class="container text-center" style="margin: 2em auto;">
+      <div class="jumbotron">
+        <h1 class="display-3">Welcome</h1>
+        <p class="lead">APLIKASI PEMUNGUTAN SUARA</p>
+    </div>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-auto mb-3">
+            <div class="card" style="width: 40rem;">
+  <img class="card-img-top" src="assets/image/prabowo.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">1. PRABOWO</h5>
+    <p class="card-text">Terwujudnya bangsa dan negara Republik Indonesia yang adil, makmur, bermartabat, relijius, berdaulat, berdiri di atas kaki sendiri di bidang ekonomi, dan berkepribadian nasional yang kuat di bidang budaya serta menjamin kehidupan yang rukun antarwarga negara tanpa memandang suku, agama, ras, latar belakang etnis dan sosial berdasarkan Pancasila dan UUD Negara Republik Indonesia Tahun 1945</p>
+    <a href="#" class="btn btn-primary">Pilih</a>
+  </div>
+</div>
+        </div>
+        <div class="col-auto mb-3">
+             <div class="card" style="width: 40rem;">
+  <img class="card-img-top" src="assets/image/prabowo.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">2. PRABOWO</h5>
+    <p class="card-text">Terwujudnya bangsa dan negara Republik Indonesia yang adil, makmur, bermartabat, relijius, berdaulat, berdiri di atas kaki sendiri di bidang ekonomi, dan berkepribadian nasional yang kuat di bidang budaya serta menjamin kehidupan yang rukun antarwarga negara tanpa memandang suku, agama, ras, latar belakang etnis dan sosial berdasarkan Pancasila dan UUD Negara Republik Indonesia Tahun 1945</p>
+    <a href="#" class="btn btn-primary">Pilih</a>
+  </div>
+</div>
+        </div>   
+    </div>
+</div>
+  </div>
+</body>
+   
